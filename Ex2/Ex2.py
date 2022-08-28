e99a600054e2a4163fd3ae0461605d8063365cbd
ViniPetra <viniciuspetratti@hotmail.com>
2022-08-28 00:44:04 -0300

    Ex 2 - O pietro me desafiou a fazer o programa em 1 linha

--- Ex2/Ex2.py
+++ Ex2/Ex2.py
@@ -1,3 +1,1 @@
-import numpy as np
-
-print(np.average(np.loadtxt("altura.txt")[np.where((np.loadtxt("anos.txt") >= 1998) & (np.loadtxt("anos.txt") <= 2005))]))
+import numpy as np;print(np.average(np.loadtxt("altura.txt")[np.where((np.loadtxt("anos.txt") >= 1998) & (np.loadtxt("anos.txt") <= 2005))]))
