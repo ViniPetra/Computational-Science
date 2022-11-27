f60ae3137829bb9d948a9940227167e5fb24e0e5
Vinicius Petratti <viniciuspetratti@hotmail.com>
2022-11-26 23:41:41 -0300

    EP2 Teste class

--- EP3/EP3.py
+++ EP3/EP3.py
@@ -1,21 +1,48 @@
-import simpy as sp
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from scipy.stats import norm, expon
-
-'''Descrição da atividade'''
+"""
+Descrição da atividade
 CASO = "Pagamento de pedágio"
 FILAS = "Carros para pagar"
 SERVICO = "Cabines abertas"
 INTEGRANTES = ["Julia mendes", "Vinicius Petratti", "Yuri Barbosa"]
+"""
 
-'''Parâmetros dos testes'''
-CARROS_POR_TEMPO = 2
-TEMPO_PEDAGIO = 20
-DESVIO_PADRAO_DO_TEMPO_PEDAGIO = 0.5
-CABINES_ABERTAS = 10
-TEMPO_DE_SIMULACAO = 200
+import simpy as sp
+import numpy as np
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+from scipy.stats import norm, expon
+
+
+class Testes:
+    def __init__(self, carros_por_tempo, tempo_pedagio, desvio_tempo_pedagio, cabines_abertas,
+                 tempo_simulacao):
+        self.carros_por_tempo = carros_por_tempo
+        self.tempo_pedagio = tempo_pedagio
+        self.desvio_tempo_pedagio = desvio_tempo_pedagio
+        self.cabines_abertas = cabines_abertas
+        self.tempo_simulacao = tempo_simulacao
+
+        np.random.seed(seed=1)
+        env = sp.Environment()
+        self.pedagios = sp.Resource(env, capacity=self.cabines_abertas)
+        env.process(chegada_dos_carros(self))
+        self.env = env
+
+    def __str__(self):
+        serie = {
+            "carros_por_tempo": self.carros_por_tempo,
+            "tempo_pedagio": self.tempo_pedagio,
+            "desvio_tempo_pedagio": self.desvio_tempo_pedagio,
+            "cabines_abertas": self.cabines_abertas,
+            "tempo_simulacao": self.tempo_simulacao
+        }
+        serie_json = json.dumps(serie)
+        return serie_json
+
+    def run_env(self):
+        self.env.run(until=self.tempo_simulacao)
+
 
 '''Listas de dados'''
 chegadas, saidas = [], []
@@ -31,8 +58,8 @@ def log_info_fila(enviroment, pedagio):
     return agora
 
 
-def distribuicao_chegada_de_carros():
-    tempo_do_proximo_carro = expon.rvs(scale=CARROS_POR_TEMPO, size=1)
+def distribuicao_chegada_de_carros(teste: Testes):
+    tempo_do_proximo_carro = expon.rvs(scale=teste.carros_por_tempo, size=1)
     return tempo_do_proximo_carro
 
 
@@ -43,47 +70,45 @@ def calcula_tempo_no_sistema(enviroment, horario_chegada):
     in_system.append(tempo_total)
 
 
-def chegada_dos_carros(enviroment):
+def chegada_dos_carros(teste: Testes):
     id_carro = 0
     while True:
-        tempo_do_proximo_carro = distribuicao_chegada_de_carros()
-        yield enviroment.timeout(tempo_do_proximo_carro)
-        tempo_de_chegada = enviroment.now
+        tempo_do_proximo_carro = distribuicao_chegada_de_carros(teste)
+        yield teste.env.timeout(tempo_do_proximo_carro)
+        tempo_de_chegada = teste.env.now
         chegadas.append(tempo_de_chegada)
         id_carro += 1
         print('%3d chegou no pedágio em %.2f' % (id_carro, tempo_de_chegada))
-        enviroment.process(cobranca(enviroment, id_carro, tempo_de_chegada))
+        teste.env.process(cobranca(id_carro, tempo_de_chegada, teste))
 
 
-def tempo_de_cobranca():
-    return norm.rvs(loc=TEMPO_PEDAGIO, scale=DESVIO_PADRAO_DO_TEMPO_PEDAGIO, size=1)
+def tempo_de_cobranca(teste: Testes):
+    return norm.rvs(loc=teste.tempo_pedagio, scale=teste.desvio_tempo_pedagio, size=1)
 
 
-def cobranca(enviroment, id_carro, horario_chegada):
-    with pedagios.request() as req:
-        print('%3d entrou na fila em %.2f' % (id_carro, env.now))
-        horario_entrada_da_fila = log_info_fila(enviroment, pedagios)
+def cobranca(id_carro, horario_chegada, teste: Testes):
+    with teste.pedagios.request() as req:
+        print('%3d entrou na fila em %.2f' % (id_carro, teste.env.now))
+        horario_entrada_da_fila = log_info_fila(teste.env, teste.pedagios)
         yield req
 
-        print('%3d saiu da fila em %.2f' % (id_carro, enviroment.now))
-        horario_saida_da_fila = log_info_fila(enviroment, pedagios)
+        print('%3d saiu da fila em %.2f' % (id_carro, teste.env.now))
+        horario_saida_da_fila = log_info_fila(teste.env, teste.pedagios)
 
         tempo_na_fila = horario_saida_da_fila - horario_entrada_da_fila
         in_queue.append(tempo_na_fila)
 
-        tempo_pesagem = tempo_de_cobranca()
-        yield enviroment.timeout(tempo_pesagem)
+        tempo_pesagem = tempo_de_cobranca(teste)
+        yield teste.env.timeout(tempo_pesagem)
         print('%3d permaneceu no sistema por %.2f' % (id_carro, tempo_pesagem))
 
-        calcula_tempo_no_sistema(enviroment, horario_chegada)
+        calcula_tempo_no_sistema(teste.env, horario_chegada)
 
 
-'''Configuração do ambiente'''
-np.random.seed(seed=1)
-env = sp.Environment()
-pedagios = sp.Resource(env, capacity=CABINES_ABERTAS)
-env.process(chegada_dos_carros(env))
-env.run(until=TEMPO_DE_SIMULACAO)
+teste1 = Testes(2, 20, 0.5, 10, 200)
+print(teste1)
+teste1.run_env()
+
 
 '''Criação dos dataframes'''
 df1 = pd.DataFrame(horarios_nas_filas, columns=['horario'])
@@ -118,15 +143,3 @@ ax2.grid()
 
 # fig.show()
 # fig2.show()
-
-'''Configuração de testes'''
-
-
-class Testes:
-    def __int__(self, CARROS_POR_TEMPO, TEMPO_PEDAGIO, DESVIO_PADRAO_DO_TEMPO_PEDAGIO, CABINES_ABERTAS,
-                TEMPO_DE_SIMULACAO):
-        self.CARROS_POR_TEMPO = CARROS_POR_TEMPO
-        self.TEMPO_PEDAGIO = TEMPO_PEDAGIO
-        self.DESVIO_PADRAO_DO_TEMPO_PEDAGIO = DESVIO_PADRAO_DO_TEMPO_PEDAGIO
-        self.CABINES_ABERTAS = CABINES_ABERTAS
-        self.TEMPO_DE_SIMULACAO = TEMPO_DE_SIMULACAO
